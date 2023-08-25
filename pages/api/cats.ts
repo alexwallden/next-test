65f65090be568862f66ac412e7dfcc0dc196b39c
alexwallden <alexander.wallden@me.com>
2023-08-25 13:54:23 +0200

    add put endpoint for updating isAdopted

--- pages/api/cats.ts
+++ pages/api/cats.ts
@@ -1,15 +1,18 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 import users from '../../MOCK_DATA.json';
-import cats from '../../cats_database.json';
-import fs from 'fs';
+// import cats from '../../cats_database.json';
+import fs, { readFileSync } from 'fs';
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET'){
-    res.status(200).json({cats})
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const cats = JSON.parse(readFileSync('cats_database.json').toString());
+  if (req.method === 'GET') {
+    console.log(cats);
+
+    res.status(200).json({ cats });
   }
-  if (req.method === 'POST') {
-
+  if (req.method === 'PUT') {
   }
-  } 
-
-  
+}
---
+++ pages/api/cats/[id].ts
@@ -0,0 +1,21 @@
+import { readFileSync, writeFile } from 'fs';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'PUT') {
+    const cats = JSON.parse(readFileSync('cats_database.json').toString());
+
+    const cat = cats.find((animal) => animal.id === req.query['id']);
+
+    cat.isAdopted = true;
+
+    writeFile('cats_database.json', JSON.stringify(cats, null, 2), (err) => {
+      if (err) console.log(err);
+    });
+
+    res.status(200).json({msg: 'Funkar'})
+  }
+}
