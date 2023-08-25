5e51037b769037f54a7b91f26b8a2ebbe02bebdd
alexwallden <alexander.wallden@me.com>
2023-08-25 12:40:33 +0200

    add cats fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
-"use client";
+'use client';
 
-import Image from 'next/image';
 import styles from './page.module.css';
 import { useEffect, useState } from 'react';
+import { IUser } from './models/IUser';
 
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<IUser[]>([]);
   useEffect(() => {
     fetch('/api/users')
       .then((response) => response.json())
       .then((data) => setUsers(data.users));
+
+      fetch('https://api.thecatapi.com/v1/images/search?api_key=live_vXNZD2Novm6IAO6VUk98KwhT9S8obSqilstHwjvIZqZdUSFl3iQIXxba00DJWDQo&limit=30')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }, []);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        {users.length > 0 && users.map((user, i) => <h1 key={i}>{user.first_name}</h1>)}
+        <ul className={styles.ul}>
+          {users.length > 0 &&
+            users.map((user, i) => <li key={i}>{user.first_name}</li>)}
+        </ul>
       </div>
     </main>
   );
